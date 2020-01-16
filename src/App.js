653733c2cd38546ffd8086ad14172f01a5c5441c
Jasmine Harris <jasminecrooms@gmail.com>
2020-01-16 15:26:26 -0800

    code changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
+const {users, companies, products} = data;
+console.log(users);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <h2>Product List({products.length}) </h2>
+      <ul>
+        {
+          products.map(product =>{
+            return (
+              <li key = {product.id}>
+                {product.name}
+              </li>
+            );
+          })
+        }
+      </ul>
+      </div>
+
+      <div>
+      <h2>User List</h2>
+      <h2>User List({users.length}) </h2>
+      <ul>
+        {
+          users.map(prouserduct =>{
+            return (
+              <li key = {user.id}>
+                {user.name}
+              </li>
+            );
+          })
+        }
+      </ul>
+      </div>
+
+  <div>
+      <h2>Company List</h2>
+      <h2>Stats</h2>
     </div>
   );
 }
